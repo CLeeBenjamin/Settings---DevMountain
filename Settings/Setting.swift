73a6da85e607a7dc11acd3d666384071c4218269
Caston  Boyd <castonboyd@Castons-MacBook-Pro.local>
2018-05-14 10:32:24 -0600

    initial commit

---
+++ Settings/Setting.swift
@@ -0,0 +1,15 @@
+//
+//  Setting.swift
+//  Settings
+//
+//  Created by Caston  Boyd on 5/14/18.
+//  Copyright © 2018 Caston  Boyd. All rights reserved.
+//
+
+import Foundation
+
+
+class Setting{
+    
+    
+}
